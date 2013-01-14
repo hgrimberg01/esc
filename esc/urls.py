0fb99b229592d3ec57f1142f0b0d2ba9a293b070
Howard Grimberg <hgrimberg@ku.edu>
2013-01-13 19:13:40 -0600

    1.Adding view to return JSON for android/mobile web client.

--- esc/urls.py
+++ esc/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-
+from django.http import HttpResponse, HttpResponseRedirect
 from adminplus import AdminSitePlus
+from scoring.views import get_team_scores
 
 admin.site = AdminSitePlus()
 admin.autodiscover()
@@ -15,7 +16,7 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+     url(r'^api/getTeamScore/(?P<team_id>\d+)/$','scoring.views.get_team_scores'),   
     # Uncomment the next line to enable the admin:
      url(r'^admin/', include(admin.site.urls)),
  
--- scoring/views.py
+++ scoring/views.py
@@ -1,1 +1,30 @@
 # Create your views here.
+from scoring.models import Score,Team
+from django.conf import settings
+from django.http import HttpResponse
+import simplejson as json
+def get_team_scores(request,team_id):
+    
+    try:
+        team = Team.objects.get(pk=team_id)
+        scores = Score.objects.filter(team=team)
+        
+        if settings.GLOBAL_SETTINGS['SCHOOL_TYPES'][0][0] == team.division :
+            division = settings.GLOBAL_SETTINGS['SCHOOL_TYPES'][0][1]
+        elif settings.GLOBAL_SETTINGS['SCHOOL_TYPES'][1][0] == team.division:
+            division = settings.GLOBAL_SETTINGS['SCHOOL_TYPES'][1][1]
+        elif settings.GLOBAL_SETTINGS['SCHOOL_TYPES'][2][0] == team.division:
+            division = settings.GLOBAL_SETTINGS['SCHOOL_TYPES'][2][1]
+        elif settings.GLOBAL_SETTINGS['SCHOOL_TYPES'][3][0] == team.division:
+            division = settings.GLOBAL_SETTINGS['SCHOOL_TYPES'][3][1]
+        ready_scores = []
+        for score in scores:
+            ready_scores.append({'event':score.event.name,'points':score.score})
+        
+        return_data = {'status':1,'team_name':team.name,'division':division,'scores':ready_scores}
+        
+    except Team.DoesNotExist:
+        return_data = {'status':0}
+        
+    print return_data
+    return HttpResponse(json.dumps(return_data), content_type="application/json")
