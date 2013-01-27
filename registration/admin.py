d5820e3957c0b037078809836953ade1e9617aa6
Howard Grimberg <hgrimberg@ku.edu>
2013-01-26 21:23:18 -0600

    Admin UI tweaks for registration

--- registration/admin.py
+++ registration/admin.py
@@ -1,13 +1,28 @@
 from registration.models import School,Teacher,Participant,Team
 from django.contrib import admin
 
-class TeamAdmin:
-    list_filter = ('name',)
+class TeamAdmin(admin.ModelAdmin):
+    list_filter = ('school','division',)
+    list_display = ('name','school','division','id')
+    search_fields = ('name','id')
 
+class SchoolAdmin(admin.ModelAdmin):
+    list_filter = ('school_type',)
+    list_display=('name','school_type',)
+    search_fields=('name',)
+
+
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display=('name',)
+    list_filter=('teacher',)
+    search_fields=('teacher','name',)
+
+    
+    
 admin.site.register(Team,TeamAdmin)
-admin.site.register(School)
+admin.site.register(School,SchoolAdmin)
 admin.site.register(Teacher)
-admin.site.register(Participant)
+admin.site.register(Participant,ParticipantAdmin)
 ## Define an inline admin descriptor for UserProfile model
 ## which acts a bit like a singleton
 #class UserProfileInline(admin.StackedInline):
