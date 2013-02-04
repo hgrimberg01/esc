c037388d268e904d070624660cfd1733642f1995
Howard Grimberg <hgrimberg@ku.edu>
2013-02-04 17:11:23 -0600

    Updated oldimport command to include event and registration import

--- registration/management/commands/oldimport.py
+++ registration/management/commands/oldimport.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 import MySQLdb as mdb2
 from registration.models import Team,Participant,School
-
+from scoring.models import Event,PreRegistration
 
 
     
@@ -46,6 +46,28 @@ class Command(BaseCommand):
                 for team in Team.objects.filter(name=team_student[1]):
                     student.teams.add(team)
                     student.save()
+                    
+        cursor.execute("SELECT `event_name` FROM `events`;")
+        event_rs = cursor.fetchall()
+        
+        Event.objects.all().delete()
+        for event in event_rs:
+            event = Event(name=event[0],max_score=100.0,min_score=0.0,event_score_type='STD')
+            event.save()
+        
+        cursor.execute("SELECT `events`.`event_name`,`team`.`team_name` FROM `events` JOIN `team_to_event` JOIN `team` WHERE `team`.`team_id`=`team_to_event`.`team_id` and `events`.`event_id`=`team_to_event`.`event_id`;")
+        event_team_rs = cursor.fetchall()
+        
+        PreRegistration.objects.all().delete()
+        for event_team in event_team_rs:
+            for team in Team.objects.filter(name=event_team[1]):
+                for event in Event.objects.filter(name=event_team[0]):
+                    reg = PreRegistration(teams=team,event=event)
+                    reg.save()
+        
+                    
+                
+                    
     
             
         
