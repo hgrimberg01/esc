3ca30011794143785955792e391902823427ef77
Howard Grimberg <hgrimberg@ku.edu>
2013-02-11 13:54:39 -0600

    Add caching for getTeams API call

--- registration/views.py
+++ registration/views.py
@@ -2,7 +2,9 @@
 from django.http import HttpResponse
 from registration.models import Team
 from django.core import serializers
+from django.views.decorators.cache import cache_page
 
+@cache_page(60 * 5)
 def get_teams(request):
     return_data =  serializers.serialize("json", Team.objects.all())
     return HttpResponse(return_data, content_type="application/json")
